07791873dff6180e1d21cb4a43f75f9f065ccb6c
Tim-byte-afk <patemkov@gmail.com>
2021-02-10 21:15:13 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import Login from '../login-screen/login-screen';
+import Favorites from '../favorites-screen/favorites-screen';
+import Offer from '../room-screen/room-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 const App = (props) => {
   return (
-    <MainScreen {...props} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact >
+          <MainScreen {...props}/>
+        </Route>
+        <Route path="/favorites" exact >
+          <Favorites />
+        </Route>
+        <Route path="/login" exact >
+          <Login />
+        </Route>
+        <Route path="/offer/:id" exact >
+          <Offer />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
